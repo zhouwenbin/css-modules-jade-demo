98e9284064ecb4559b8e4d84f3c2b390219add21
zhouwenbin <330759294@qq.com>
2015-12-25 22:28:04 +0800

    add

---
+++ README.md
@@ -0,0 +1,7 @@
+
+## Install
+
+```
+npm install
+npm start
+```
--- entry.js
+++ entry.js
@@ -1,3 +1,2 @@
-require("!style!css?modules!./css/styles.css");
-var template = require("!jade!./css/style.jade");
+var template = require("./src/style.jade");
 document.write(template());
